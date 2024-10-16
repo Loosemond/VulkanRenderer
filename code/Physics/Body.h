7746e74fe63b81a8e1c37a5ac7691b78b9d18989
Joao Ribeiro <makaan77@gmail.com>
2024-10-16 22:19:22 +0100

    My First collision

--- code/Physics/Body.h
+++ code/Physics/Body.h
@@ -7,21 +7,75 @@
 #include "../Math/Matrix.h"
 #include "../Math/Bounds.h"
 #include "Shapes.h"
+#include <random>
+
 
 #include "../Renderer/model.h"
 #include "../Renderer/shader.h"
+#include <string>
 
 /*
 ====================================================
 Body
 ====================================================
 */
+
+
 class Body {
+
+
 public:
 	Body();
 
+	enum body_t
+	{
+		BODY_FIXED,
+		BODY_DYNAMIC
+	};
+
+	body_t		m_type;
 	Vec3		m_position;
 	Quat		m_orientation;
-
+	Vec3		m_linearVelocity;
+	float		m_invMass;
 	Shape *		m_shape;
+	float		m_radius;
+	float		m_id;
+
+	void ApplyImpulseLinear(const Vec3& impulse)
+	{
+		if (0.0f != m_invMass)
+		{
+			// p = mv
+			// dp = m dv = J
+			// => dv = J / m
+			m_linearVelocity += impulse * m_invMass;
+		}
+	}
+
+	Vec3 DistanceTo(Vec3 targetObject)
+	{
+		if (m_position.GetLengthSqr() > targetObject.GetLengthSqr()) 
+		{
+			return m_position - targetObject;
+		}
+		else {
+			return targetObject - m_position;
+		}
+	}
+
+	void CollisionCheck(Body targetBody) 
+	{
+
+		if (targetBody.m_shape->GetType() == Shape::SHAPE_SPHERE)
+		{
+			 printf("Body %f Distance to %f: %f\n", m_id, targetBody.m_id, (DistanceTo(targetBody.m_position).GetLengthSqr()));
+			if ((DistanceTo(targetBody.m_position).GetLengthSqr() ) < ((m_radius + targetBody.m_radius)* (m_radius + targetBody.m_radius)))
+			{
+				m_linearVelocity = Vec3{ 0,0,0 };
+			}
+		}
+	}
+
 };
+
--- code/Scene.cpp
+++ code/Scene.cpp
@@ -47,15 +47,32 @@ Scene::Initialize
 */
 void Scene::Initialize() {
 	Body body;
-	body.m_position = Vec3( 0, 0, 0 );
+	body.m_id = 0;
+	body.m_type = Body::BODY_DYNAMIC;
+	body.m_position = Vec3( 0, 0, 1 );
 	body.m_orientation = Quat( 0, 0, 0, 1 );
 	body.m_shape = new ShapeSphere( 1.0f );
+	body.m_radius = 1.0f;
+	body.m_invMass = 1/10.0f;
 	m_bodies.push_back( body );
 
+	body.m_id = 1;
+	body.m_type = Body::BODY_DYNAMIC;
 	body.m_position = Vec3( 0, 0, -101 );
 	body.m_orientation = Quat( 0, 0, 0, 1 );
 	body.m_shape = new ShapeSphere( 100.0f );
+	body.m_radius = 100.0f;
+	body.m_invMass = 0.0f;
 	m_bodies.push_back( body );
+
+	body.m_id = 2;
+	body.m_type = Body::BODY_DYNAMIC;
+	body.m_position = Vec3(0, 0, 4);
+	body.m_orientation = Quat(0, 0, 0, 1);
+	body.m_shape = new ShapeSphere(1.0f);
+	body.m_radius = 1.0f;
+	body.m_invMass = 1 / 10.0f;
+	m_bodies.push_back(body);
 
 	// TODO: Add code
 }
@@ -67,4 +84,23 @@ Scene::Update
 */
 void Scene::Update( const float dt_sec ) {
 	// TODO: Add code
+	for (auto& body : m_bodies)
+	{
+		if (body.m_type != Body::BODY_FIXED)
+		{
+			// Gravity is an impulse
+			// I = dp, F = dp/dt => dp = f * dp => I = F * dt
+			// F = mgs
+			Vec3 impulseGravity = Vec3(0, 0, -10) * 1/body.m_invMass * dt_sec;
+			body.ApplyImpulseLinear(impulseGravity);
+			for (auto& contact_body : m_bodies)
+			{
+				if (body.m_id != contact_body.m_id) 
+				{
+					body.CollisionCheck(contact_body);
+				}
+			}
+			body.m_position += body.m_linearVelocity * dt_sec;
+		}
+	}
 }
